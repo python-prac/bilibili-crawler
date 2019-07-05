17c0fee91416ae08892caf6026fbd4aa788a8c58
Ameame123 <1700012966@pku.edu.cn>
2019-07-05 13:11:03 +0800

    加入了proxy参数

--- get_info.py
+++ get_info.py
@@ -21,25 +21,31 @@ class Project(object):
         self.S = requests.Session()
         self.S.mount('https://', HTTPAdapter(max_retries=max_retries))
 
-    def url_get(self, url, sleep_time=0.5):
+    def url_get(self, url, sleep_time=0.5, proxy_ip=None):
         """
         爬虫
         :param url:输入的url
         :param sleep_time:每次抓取的暂停时间
+        :param proxy_ip: 代理服务器IP
         :return: 成功则返回utf-8格式的字符串，失败返回None
         """
         try:
-            response = self.S.get(url, headers=headers)
+            if proxy_ip:
+                proxies = {"http":"http://" + proxy_ip, "https":"https://" + proxy_ip}
+                response = self.S.get(url, headers=headers, proxies=proxies)
+            else:
+                response = self.S.get(url, headers=headers)
             time.sleep(sleep_time)
             return response.content.decode('utf-8')
         except:
             return None
 
 
+
 proj = Project()
 for ID in range(2, 400000000+2+1, 40000):
     url = 'https://api.bilibili.com/x/relation/followings?vmid=%d&pn=1&ps=50&order=desc' % ID
-    print(proj.url_get(url, 1))
+    print(proj.url_get(url, sleep_time=1))
 
 
 
